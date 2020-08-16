64c826ea7067a67b82f4fe163b7d61e37bdb0640
Yuriy Rybak <rybak.yuriy.m@gmail.com>
2020-08-16 15:54:24 +0300

    add changes v1

--- filecrush2/compaction-spark/src/main/scala/com/epam/Compact.scala
+++ filecrush2/compaction-spark/src/main/scala/com/epam/Compact.scala
@@ -2,45 +2,52 @@ package com.epam
 
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
-import org.apache.hadoop.io.compress.CompressionCodecFactory
-import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.sql.SparkSession
 
 object Compact extends App {
 
 
   private var codecFactory = null
-//  private[spark_compaction] var fs = null
-//  private[spark_compaction] var fsArray = null
+  //  private[spark_compaction] var fs = null
+  //  private[spark_compaction] var fsArray = null
 
-  import org.apache.hadoop.fs.FileStatus
-  import org.apache.hadoop.fs.FileSystem
   import java.net.URI
+
+  import org.apache.hadoop.fs.FileSystem
 
 
   override def main(args: Array[String]): Unit = {
 
+
     val spark = SparkSession
       .builder()
       .master("local")
-//      .config("spark.hadoop.dfs.client.use.datanode.hostname", "true")
+      //      .config("spark.hadoop.dfs.client.use.datanode.hostname", "true")
       .appName("Spark Compaction")
       .getOrCreate()
 
 
-//    val path="hdfs://localhost:8020/topics/scala_confluent/year=2020/month=08/day=11/hour=01/scala_confluent+0+0032017843+0032044290.avro"
-    val path=args(0)
+    //    val path="hdfs://localhost:8020/topics/scala_confluent/year=2020/month=08/day=11/hour=01/scala_confluent+0+0032017843+0032044290.avro"
+    val path = args(0)
     val conf = new Configuration()
+    conf.addResource(new Path("file:///etc/hadoop/conf/core-site.xml"));
+    conf.addResource(new Path("file:///etc/hadoop/conf/hdfs-site.xml"));
     conf.set("fs.hdfs.impl", classOf[org.apache.hadoop.hdfs.DistributedFileSystem].getName)
-//    conf.set("fs.file.impl", classOf[org.apache.hadoop.fs.LocalFileSystem].getName)
+    //    conf.set("fs.file.impl", classOf[org.apache.hadoop.fs.LocalFileSystem].getName)
     val fs = FileSystem.get(URI.create(path), conf)
     val status = fs.getFileStatus(new Path(path))
 
-  println(status.getLen)
-  println(status.getPermission)
+
+    val avroFiles = spark.read.format("com.databricks.spark.avro").load(path)
+    avroFiles.show(3)
+
+    println("File Size(KBites)= "+status.getLen/1024)
+    println("Block Size(KBites)= "+status.getBlockSize/1024)
+    println(status.getPermission)
+    println("Is Dir= " + status.isDirectory)
+
 
   }
-
-
 
 
 }
